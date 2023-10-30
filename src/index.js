b5c8590ac2cd1edde6a316d07b720adab62b3850
michaelOch <ochuenwikechukwudi@hotmail.com>
2023-10-30 19:00:44 +0100

    fix: trying to fix cor errors

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: "*",
+    origin: "https://cherrystore-xfki.onrender.com",
     methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"],
 }));
 
